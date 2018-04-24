53714ed14b0c85f90bfd7490ab3de07efedf9830
beeceej <beeceej.code@gmail.com>
2018-04-24 10:07:12 -0500

    don't panic on serve

--- cmd/courier/main.go
+++ cmd/courier/main.go
@@ -11,15 +11,21 @@ import (
 )
 
 func main() {
-	lis, err := net.Listen("tcp", courier.CourierPort)
-	if err != nil {
-		log.Fatalf("failed to listen: %v", err)
+	var (
+		lis net.Listener
+		err error
+	)
+	if lis, err = net.Listen("tcp", courier.CourierPort); err != nil {
+		log.Fatalf("failed to listen: %v\n", err)
+	} else {
+		log.Printf("Listening on %v\n", lis.Addr())
 	}
+
 	s := grpc.NewServer()
 	pb.RegisterMailServer(s, &courier.Courier{})
 
 	reflection.Register(s)
-	if err := s.Serve(lis); err != nil {
-		log.Fatalf("failed to serve: %v", err)
+	if err = s.Serve(lis); err != nil {
+		log.Printf("failed to serve: %v\n", err)
 	}
 }
